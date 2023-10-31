c65fea42a3e16b1c856f24f1aad9ea1e776e0031
Mister_YR <yaroslavryabov@hotmail.com>
2023-10-31 15:47:54 +0200

    add folder with smart_hdd_monitor

---
+++ smart_disk_monitoring/README.md
@@ -0,0 +1,2 @@
+.sh script for monitoring disk smart status and push notification via telegram if disk healthcheck: FAILED
+Using smartmontools utils
---
+++ smart_disk_monitoring/smart.sh
@@ -0,0 +1,30 @@
+if ! command -v smartctl &> /dev/null; then
+    echo "smartmontools is not installed. Installing..."
+    sudo apt-get update
+    sudo apt-get install smartmontools
+else
+    echo "smartmontools is already installed."
+fi
+for device in /dev/sd*; do
+  echo "=== SMART status for $device ==="
+  sudo smartctl -l selftest "$device"
+done
+sleep 10
+date=$(date) 
+echo '#######' "$date" '########' >> smart_log.txt
+for device in /dev/sd*; do
+  echo "=== SMART status for $device ===" >> smart_log.txt
+  status=$(sudo smartctl -H "$device" | grep -E "SMART status for*|SMART overall-health *" )
+  echo "$status" >> smart_log.txt
+done
+#cat smart_log.txt | grep  "SMART overall-health self-assessment test result: FAILED"
+if grep -q "SMART overall-health self-assessment test result: FAILED" smart_log.txt; then
+ #!/bin/bash
+        TOKEN="TOKEN_FOR_TELEGRAM"
+        CHAT_ID="TELEGRAM_CHAT_ID"
+        #traf_stat=$(vnstat -i eth0 | grep total | awk '{ print $8  }' | head -n -2)
+                #traf_stat=$(vnstat -i eth0 -m | grep "GiB" | awk '{ print $8}' | awk 'NR==3')
+        smart=$(tail -n 7 smart_log.txt)
+        # %0A print from new row
+        curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="Some_disks_has_en_ERRORS: %0A $smart" > /dev/null
+fi
